64f97c696ba18e761d12907e7ce684e73d49b8a1
albin-baby-2002 <albinbtg@gmail.com>
2023-08-17 04:48:30 +0530

    committed

---
+++ index.js
@@ -0,0 +1,91 @@
+const toggler = document.getElementById("toggle-button");
+
+const nav = document.getElementById("navigation-Bar");
+
+toggler.addEventListener("click", () => {
+  nav.classList.toggle("hidden");
+
+  if (toggler.classList.contains("fa-bars")) {
+    toggler.classList.replace("fa-bars", "fa-xmark");
+  } else {
+    toggler.classList.replace("fa-xmark", "fa-bars");
+  }
+});
+
+// code for contact form
+
+// const form = document.getElementById("contact-form");
+
+// const names = document.getElementById("names");
+// const email = document.getElementById("email");
+// const submitBtn = document.getElementById("submit-button");
+// const message = document.getElementById("message");
+
+// function validateName() {
+//   const err = names.parentElement.querySelector("#error");
+
+//   if (names.value.length == 0) {
+//     err.innerHTML = "Enter your name";
+//     return false;
+//   } else if (!names.value.match(/^[A-Za-z]+\s{1}[A-Za-z]+$/)) {
+//     err.innerHTML = "Enter Full Name";
+//     return false;
+//   } else {
+//     err.innerHTML = "";
+//     return true;
+//   }
+// }
+
+// function validateMail() {
+//   const err = email.parentElement.querySelector("#error");
+
+//   if (email.value.length == 0) {
+//     err.innerHTML = "Enter your email";
+//     return false;
+//   } else if (
+//     !email.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+//   ) {
+//     err.innerHTML = "Enter valid email";
+//     return false;
+//   } else {
+//     err.innerHTML = "";
+//     return true;
+//   }
+// }
+
+// function validateMessage() {
+//   const err = message.parentElement.querySelector("#error");
+
+//   if (message.value.length < 10) {
+//     err.innerHTML = "min length is 10 ";
+//     return false;
+//   } else {
+//     err.innerHTML = "";
+//     return true;
+//   }
+// }
+
+// form.addEventListener("submit", (e) => {
+//   if (names.value.length == 0) {
+//     e.preventDefault();
+
+//     const err = names.parentElement.querySelector("#error");
+//     err.innerHTML = "Enter your name";
+//   }
+
+//   if (email.value == "" || email.value == null) {
+//     e.preventDefault();
+//     const err = email.parentElement.querySelector("#error");
+//     err.innerHTML = "Enter an email id";
+//   }
+
+//   if (!validateMessage() || !validateName() || !validateMail()) {
+//     e.preventDefault();
+//   }
+// });
+
+// submitBtn.addEventListener("click", () => {
+//   if (validateMail() && validateName() && validateMessage()) {
+//     window.alert("Submitted successfullly");
+//   }
+// });
